39a4021716e9f1a974e52d45bb99dd852e00df06
Laurence Tuck <laurence@innate.co.za>
2014-03-12 20:48:37 +0200

    removed css

--- domain-mapping-autofill.php
+++ domain-mapping-autofill.php
@@ -10,8 +10,7 @@ function add_domain_mapping_autofill() {
 	if(isset($_GET['page']) && $_GET['page'] == 'dm_domains_admin'){
 		wp_enqueue_script( 'jquery' );
 		wp_enqueue_script( 'jquery-ui-autocomplete' );
-	    wp_enqueue_style( 'jquery_css', 'http://code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css');
-		wp_enqueue_script( 'jquery-domain_mapping_autofill', plugin_dir_url( __FILE__ ) . 'js/domain_mapping_autofill.js' );
+	    wp_enqueue_script( 'jquery-domain_mapping_autofill', plugin_dir_url( __FILE__ ) . 'js/domain_mapping_autofill.js' );
 	}  	         
 }
 // create autocomplete array
